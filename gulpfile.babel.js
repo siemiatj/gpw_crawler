fa13771f397592e286812feb85b705b9338a4a4e
siemiatj <kuba@saucelabs.com>
2017-03-28 21:31:58 +0200

    - add gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,48 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import del from 'del';
+import runSequence from 'run-sequence';
+import gulpEslint from 'gulp-eslint';
+import nodemon from 'gulp-nodemon';
+
+var config = {
+  paths: {
+    js: {
+      src: 'src/**/*.es6',
+      dist: 'dist/'
+    }
+  }
+};
+
+gulp.task('clean', () =>
+  del(config.paths.js.dist)
+);
+
+gulp.task('babel', ['babel-src']);
+
+gulp.task('babel-src', ['lint-src'], () =>
+  gulp.src(config.paths.js.src)
+    .pipe(babel())
+    .pipe(gulp.dest(config.paths.js.dist))
+);
+
+gulp.task('lint-src', () =>
+  gulp.src(config.paths.js.src)
+    .pipe(gulpEslint())
+    .pipe(gulpEslint.format())
+    .pipe(gulpEslint.failAfterError())
+);
+
+gulp.task('build', () => {
+  gulp.src(config.paths.js.src)
+    .pipe(babel())
+    .pipe(gulp.dest(config.paths.js.dist));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(config.paths.js.src, ['babel-src']);
+});
+
+gulp.task('default', () =>
+  runSequence('clean', ['babel']) //, 'server') // we're not using happy so don't run this
+);
